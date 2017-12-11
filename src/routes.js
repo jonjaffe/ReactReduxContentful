230998f69b78fd421c3c504d84e4b56674d7703a
jonjaffe <jonjaffe5@gmail.com>
2017-12-11 09:14:13 -0800

    Renders posts index initial

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
+import PostsIndex from './components/posts_index';
 
 import App from './components/app';
 
 export default (
   <Route path="/" component={App}>
+    <IndexRoute component={PostsIndex}/>
   </Route>
 );
