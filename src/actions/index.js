ed514b62f516858af53a93c483c82d930c80343c
jonjaffe <jonjaffe5@gmail.com>
2017-12-11 16:50:18 -0800

    Renders featured img for each entry

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const FETCH_ASSET = 'FETCH_ASSET';
 const API_BASE_URL = 'https://cdn.contentful.com';
 export const API_SPACE_ID = 'qd9j4ldrs8tz';
 export const API_TOKEN = '908a53956b02fcc278b08e67ce3c0dbe0403307a8cd5273a4020994c360012d3';
@@ -9,6 +10,15 @@ export function fetchPosts() {
   const request = axios.get(`${API_BASE_URL}/spaces/${API_SPACE_ID}/entries?access_token=${API_TOKEN}`);
   return {
     type: FETCH_POSTS,
+    payload: request
+  };
+}
+
+export function fetchAsset(id) {
+  const request = axios.get(`${API_BASE_URL}/spaces/${API_SPACE_ID}/assets/${id}?access_token=${API_TOKEN}`);
+
+  return {
+    type: FETCH_ASSET,
     payload: request
   };
 }
--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPosts, API_SPACE_ID, API_TOKEN } from '../actions/index';
+import { fetchPosts, fetchAssets, API_SPACE_ID, API_TOKEN } from '../actions/index';
+import Asset from './asset';
 
 class PostsIndex extends Component {
   componentWillMount() {
@@ -10,24 +11,26 @@ class PostsIndex extends Component {
   renderPosts() {
     return this.props.posts.map((post, index) => {
       return (
-        <div key={post.sys.id}>
+        <div key={post.sys.id} className='post_index_item'>
           <h3>{post.fields.title}</h3>
           <p>{post.fields.description}</p>
-          
+          <Asset assetId={post.fields.featuredImage.sys.id} />
         </div>
       );
     });
   }
   render() {
     return (
-      <div>
+      <div className>
         <h2>Blog Posts</h2>
-        {this.renderPosts()}
+        <div className='post_index_container'>
+          {this.renderPosts()}
+        </div>
       </div>
     );
   }
 }
 function mapStateToProps(state) {
-  return { posts: state.posts.all };
+  return { posts: state.posts.all, images: state.posts.images };
 }
 export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
